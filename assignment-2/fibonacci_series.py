a8bca7a18a95639b2265869dfa53752c3e5be53b
priyankabaskaran1334 <Priyankairis64@gmail.com>
2025-06-23 13:27:59 +0530

    Moved fibonacci_series.py into assignment-2

---
+++ assignment-2/fibonacci_series.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    fib_list = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_list.append(a)
+        a, b = b, a + b
+    return fib_list
+n = 10
+result = fibonacci(n)
+print("Fibonacci sequence:", result)
